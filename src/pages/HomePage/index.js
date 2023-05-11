cd34fef628f6a85ad2819f00936bc19ce404262f
aigerim055 <omurzakova727@gmail.com>
2023-05-11 11:02:28 +0600

    header and main-section

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Header from "../../components/header";
+import Img from '../../components/images/main-img.png'
+import './index.css'
 
 const HomePage = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header/>
+            <section className="main-section">
+                <div className="container">
+                    <h1>МАКАРОННАЯ</h1>
+                    <div className="main-ingredients">
+                        <img src={Img} alt=""/>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
