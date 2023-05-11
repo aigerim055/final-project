8b464d5cf504f818ac4c77b8c8d38996cf5b741f
aigerim055 <omurzakova727@gmail.com>
2023-05-11 14:32:17 +0600

    fix header and add redux

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from "../../components/header";
 import Img from '../../components/images/main-img.png'
 import './index.css'
+import {useSelector} from "react-redux";
+
 
 const HomePage = () => {
+
+
+    const popular_meals = useSelector(state => state.popular_meals)
+    console.log(popular_meals)
+
     return (
         <>
-            <Header/>
+
             <section className="main-section">
+                <Header/>
                 <div className="container">
                     <h1>МАКАРОННАЯ</h1>
                     <div className="main-ingredients">
---
+++ src/redux/action/mealAction.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export const getPopularMeals = () => {
+    return (dispatch) => {
+        axios('https://645bdb7a99b618d5f327dbf0.mockapi.io/macaronnaya/categories')
+            .then(({data}) => {
+                dispatch({type: 'GET_POPULAR_MEALS', payload: data})
+            })
+    }
+}
