800ba4d8eaa5103fe3cd8890377f0dbb09ce2ef2
aigerim055 <omurzakova727@gmail.com>
2023-05-12 06:07:12 +0600

    category cards

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import './index.css'
 import {useSelector} from "react-redux";
+import CategoryItem from "../categoryItem";
+import Img1 from './images/img_1.png'
+import Img2 from './images/img_2.png'
+import Img3 from './images/img_3.png'
+import Img4 from './images/img_4.png'
+import Img5 from './images/img_5.png'
+import Img6 from './images/img_6.png'
+import Img7 from './images/img_7.png'
+import Img8 from './images/img_8.png'
+import Img9 from './images/img_9.png'
 
 const Categories = () => {
-
-
-
 
     return (
         <section>
             <div className="container">
                 <h2 className={'subtitle'}>Категории блюд</h2>
                 <div className="row">
-                    {}
+                    <div className={'col-4'}>
+                        <CategoryItem name={'завтраки'} img={Img1}/>
+                    </div>
+                    <div className={'col-4'}>
+                        <CategoryItem name={'супы'} img={Img2}/>
+                    </div>
+                    <div className={'col-4'}>
+                        <CategoryItem name={'Горячие блюдо'} img={Img3}/>
+                    </div>
+                    <div className={'col-4'}>
+                        <CategoryItem name={'Паста и Ризотто'} img={Img4}/>
+                    </div>
+                    <div className={'col-4'}>
+                        <CategoryItem name={'Закуски и гарниры'} img={Img5}/>
+                    </div>
+                    <div className={'col-4'}>
+                        <CategoryItem name={'Детское меню'} img={Img6}/>
+                    </div>
+                    <div className={'col-4'}>
+                        <CategoryItem name={'Салаты'} img={Img7}/>
+                    </div>
+                    <div className={'col-4'}>
+                        <CategoryItem name={'Соусы'} img={Img8}/>
+                    </div>
+                    <div className={'col-4'}>
+                        <CategoryItem name={'Напитки'} img={Img9}/>
+                    </div>
                 </div>
             </div>
         </section>
--- src/redux/action/mealAction.js
+++ src/redux/action/mealAction.js
@@ -4,16 +4,8 @@ export const getPopularMeals = () => {
     return (dispatch) => {
         axios('https://645bdb7a99b618d5f327dbf0.mockapi.io/macaronnaya/categories')
             .then(({data}) => {
-                dispatch({type: 'GET_POPULAR_MEALS', payload: data})
+                dispatch({type: 'GET_MEALS', payload: data[0]})
             })
     }
 }
 
-export const getCategories = () => {
-    return (dispatch) => {
-        axios('https://645bdb7a99b618d5f327dbf0.mockapi.io/macaronnaya/categories')
-            .then(({data}) => {
-                dispatch({type: 'GET_CATEGORIES', payload: data})
-            })
-    }
-}
